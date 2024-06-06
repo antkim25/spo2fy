6887f0ff7c79a6cdfd1f45b1bc74e3a4672265db
matchka <myung.yaejun@gmail.com>
2024-06-06 19:42:35 -0400

    Dhyan and I implemented pausing, unpausing, toggling pausing/unpausing, and skipping.

--- spo2fy.py
+++ spo2fy.py
@@ -1,28 +1,94 @@
 import time, random, os
 from pygame import mixer
+from threading import Thread
+
 sys_random = random.SystemRandom()
 mixer.init()
 
 playlist = "study"
 
-def underscoreRemover(s):
-    newS = ""
-    for i in range (0, len(s)):
-        if s[i] != "_":
-            newS += s[i]
-        else:
-            newS += " "
-    return newS
+class Player:
+    def __init__(self, paused):
+        print("----------------------------")
+        print("Spotify 2 (Version Beta 1.0)")
+        print("----------------------------")
+        print("Basic Commands")
+        print("Pause: p")
+        print("Unpause: u")
+        print("Toggle: t")
+        print("Skip: s")
+        print("----------------------------")
+        self.paused = paused
 
-while True:
-    song = sys_random.choice(os.listdir(playlist))
-    songPath = playlist + "/" + song
-    songSound = mixer.Sound(songPath)
-    mixer.music.load(songPath)
-    mixer.music.set_volume(0.7)
-    mixer.music.play()
+    def underscoreRemover(self, s):
+        newS = ""
+        for i in range (0, len(s)):
+            if s[i] != "_":
+                newS += s[i]
+            else:
+                newS += " "
+        return newS
 
-    leftParen = song.index("(")
-    print("Currently Playing: " + underscoreRemover(song[0:leftParen]) + "by " + underscoreRemover(song[leftParen+1:-5]))
+    def songPick(self):
+        song = sys_random.choice(os.listdir(playlist))
+        songPath = playlist + "/" + song
+        return song, songPath
 
-    time.sleep(songSound.get_length())
+    def songPlayer(self):
+        song, songPath = self.songPick()
+        mixer.music.load(songPath)
+        mixer.music.set_volume(0.7)
+        mixer.music.play()
+
+        leftParen = song.index("(")
+        print("Currently Playing: " + self.underscoreRemover(song[0:leftParen]) + "by " + self.underscoreRemover(song[leftParen+1:-5]))
+
+    def skip(self):
+        song, songPath = self.songPick()
+        mixer.music.load(songPath)
+        mixer.music.set_volume(0.7)
+        mixer.music.play()
+
+        leftParen = song.index("(")
+        print("Currently Playing: " + self.underscoreRemover(song[0:leftParen]) + "by " + self.underscoreRemover(song[leftParen+1:-5]))
+
+
+    def inputChecker(self):
+        while True:
+            time.sleep(1.1)
+            x = input()
+            if (x == "Pause" or x == "pause" or x == "p"):
+                self.paused = True
+                print("Paused!")
+                mixer.music.pause()
+            elif (x == "Unpause" or x == "unpause" or x == "u"):
+                self.paused = False
+                print("Unpaused!")
+                mixer.music.unpause()
+            elif (x == "T" or x == "t"):
+                if mixer.music.get_busy():
+                    self.paused = True
+                    print("Paused!")
+                    mixer.music.pause()
+                else:
+                    self.paused = False
+                    print("Unpaused!")
+                    mixer.music.unpause()
+            elif (x == "Skip" or x == "skip" or x == "s"):
+                print("Skipped!")
+                self.skip()
+
+    def playChecker(self):
+        while True:
+            time.sleep(1)
+            if not self.paused and not mixer.music.get_busy():
+                self.skip()
+
+paused = False
+player = Player(paused)
+songPlay = Thread(target = player.songPlayer)
+inputCheck = Thread(target = player.inputChecker)
+playCheck = Thread(target = player.playChecker)
+songPlay.start()
+inputCheck.start()
+playCheck.start()
